927c710fd0bdd151c8ef68f417ebe3780f436b1b
vzsoares <jogolots2@gmail.com>
2022-04-28 11:34:45 -0300

    fea:github link

--- packages/components/CalendarSection.js
+++ packages/components/CalendarSection.js
@@ -1,25 +1,49 @@
 import Calendar from "./Calendar";
 import { CalendarContextProvider } from "../features/calendarContext";
+import { FaGithubSquare } from "react-icons/fa";
 import { colors } from "../features/calendarStyles";
 
 export default function CalendarSection() {
   return (
-    <main style={{ backgroundColor: colors.backgroundBlack }}>
+    <main
+      style={{
+        backgroundColor: colors.backgroundBlack,
+        height: "100vh",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+      }}
+    >
       <div style={{ maxWidth: "90vw", margin: "0 auto" }}>
-        <div
-          style={{
-            height: "100vh",
-            maxWidth: "900px",
-            margin: "0 auto",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
+        <div style={{}}>
           <CalendarContextProvider>
             <Calendar />
           </CalendarContextProvider>
         </div>
+      </div>
+      {/* gitHub ref */}
+      <div
+        className='github'
+        style={{
+          textAlign: "center",
+          justifyItems: "flex-end",
+        }}
+      >
+        <a
+          href='https://github.com/vzsoares/Calendar'
+          target='_blank'
+          rel='noreferrer'
+          className='github'
+        >
+          <FaGithubSquare
+            style={{
+              color: colors.darkGrey,
+              fontSize: "3rem",
+              marginTop: "0.5rem",
+            }}
+          />
+        </a>
       </div>
     </main>
   );
